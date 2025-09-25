c310db51a8acf04b3e0738e5438ff4a051056027
dmswls <56065660+cej3827@users.noreply.github.com>
2025-09-25 20:39:58 +0900

    update

--- src/routes/bookmarks.js
+++ src/routes/bookmarks.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getBookmarks, addBookmark, deleteBookmark } = require('../controllers/bookmarkController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // router.get('/:categoryId', getCategoryBookmarks)
 router.get('/', getBookmarks);
-router.post('/', addBookmark);
+router.post('/', authMiddleware, addBookmark);
 router.delete('/:id', deleteBookmark);
 
 module.exports = router;
